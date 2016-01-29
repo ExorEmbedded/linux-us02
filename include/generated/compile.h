0067d7855e2312bf91dc60c85b7a1e394cbc65a7
Giovanni Pavoni <gpavoni@Ubuntu1204-giovanni.(none)>
2016-01-29 16:07:59 +0100

    Removed temporary and unnecessary files

--- include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 3 */
-/* SMP PREEMPT RT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#3 SMP PREEMPT RT Thu Dec 3 08:34:11 CET 2015"
-#define LINUX_COMPILE_BY "gpavoni"
-#define LINUX_COMPILE_HOST "Ubuntu1204-giovanni"
-#define LINUX_COMPILER "gcc version 4.7.3 20121106 (prerelease) (crosstool-NG linaro-1.13.1-4.7-2012.11-20121123 - Linaro GCC 2012.11) "
