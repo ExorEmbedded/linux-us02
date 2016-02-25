ff9e81baa9f9e5ddf0b0afb646def01d22c71df4
Giovanni Pavoni <gpavoni@Ubuntu1204-giovanni.(none)>
2016-02-25 11:11:01 +0100

    US02 eTOP ------------------ - Added support for FPGA updating (QSPI flash programming through HPS)

--- drivers/mtd/spi-nor/spi-nor.c
+++ drivers/mtd/spi-nor/spi-nor.c
@@ -950,6 +950,40 @@ static int micron_quad_enable(struct spi_nor *nor)
 	return 0;
 }
 
+static int micron_set_dummy_cycles(struct spi_nor *nor, u8 dummy_cycles)
+{
+	int ret;
+	u8 val, mask;
+
+	/* Read the Volatile Configuration Register (VCR). */
+	ret = nor->read_reg(nor, SPINOR_OP_RD_VCR, &val, 1);
+	if (ret < 0) 
+	{
+		dev_err(nor->dev, "error %d reading VCR\n", ret);
+		return ret;
+	}
+
+	write_enable(nor);
+
+	/* Update the number of dummy into the VCR. */
+	mask = GENMASK(7, 4);
+	val &= ~mask;
+	val |= (dummy_cycles << 4) & mask;
+	ret = nor->write_reg(nor, SPINOR_OP_WR_VCR, &val, 1, 0);
+	if (ret < 0) {
+		dev_err(nor->dev, "error while writing VCR register\n");
+		return ret;
+	}
+	
+	write_disable(nor);
+
+	ret = spi_nor_wait_till_ready(nor);
+	if (ret)
+		return ret;
+
+	return 0;
+}
+
 static int set_quad_mode(struct spi_nor *nor, struct flash_info *info)
 {
 	int status;
@@ -1157,6 +1191,11 @@ int spi_nor_scan(struct spi_nor *nor, const char *name, enum read_mode mode)
 	}
 
 	nor->program_opcode = SPINOR_OP_PP;
+	
+	/* Micron Flashes have default 15 read dummy cycles, so set this value to 8
+	 */
+	if (JEDEC_MFR(info) == CFI_MFR_ST)
+	  micron_set_dummy_cycles(nor, 8);
 
 	if (info->addr_width)
 		nor->addr_width = info->addr_width;
